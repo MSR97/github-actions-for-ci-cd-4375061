74a2ed14ca83084f14b3604ea43b58b78c974009
Mohammad Resshadati <mohammad.reshadati@postnl.nl>
2024-11-02 18:33:13 +0100

    Ruff Added

--- Ch00/main.py
+++ Ch00/main.py
@@ -1,10 +1,6 @@
 def func(input_stream):
-                a = [i for i in range(input_stream)]
-                return a
-
-
-
-
+    a = list(range(input_stream))
+    return a
 
 
 print(func(10))
